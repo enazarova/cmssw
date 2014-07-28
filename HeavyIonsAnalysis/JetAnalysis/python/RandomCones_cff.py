90a3b8e9f6e82400c0501cf3c58ce8c730cd8a2f
Raghav Kunnawalkam Elayavalli <raghav.k.e@cern.ch>
2014-07-28 10:32:12 -0400

    testing the RandomCones branch part 2

--- HeavyIonsAnalysis/JetAnalysis/python/RandomCones_cff.py
+++ HeavyIonsAnalysis/JetAnalysis/python/RandomCones_cff.py
@@ -3,7 +3,7 @@ from RecoHI.HiJetAlgos.HiPFJetParameters_cff import *
 from RecoHI.HiJetAlgos.HiCaloJetParameters_cff import *
 
 akPu1PFCones = cms.EDProducer(
-    "HiInclusiveJetAnalyzer",
+    "JetAlgorithmAnalyzer",
     HiPFJetParameters,
     AnomalousCellParameters,
     MultipleAlgoIteratorBlock,
@@ -32,33 +32,37 @@ akPu1PFCones = cms.PSet(
     engineName = cms.untracked.string('TRandom3')
   )
 
-akPu2PFCones = akPu1PFCones.clone(rParam = 0.2, patJetSrc = cms.untracked.InputTag("akPu2PFpatJets"))
-akPu3PFCones = akPu1PFCones.clone(rParam = 0.3, patJetSrc = cms.untracked.InputTag("akPu3PFpatJets"))
-akPu4PFCones = akPu1PFCones.clone(rParam = 0.4, patJetSrc = cms.untracked.InputTag("akPu4PFpatJets"))
-akPu5PFCones = akPu1PFCones.clone(rParam = 0.5, patJetSrc = cms.untracked.InputTag("akPu5PFpatJets"))
-akPu6PFCones = akPu1PFCones.clone(rParam = 0.6, patJetSrc = cms.untracked.InputTag("akPu6PFpatJets"))
+akPu2PFCones = akPu1PFCones.clone(rParam = cms.double(0.2), patJetSrc = cms.untracked.InputTag("akPu2PFpatJets"))
+akPu3PFCones = akPu1PFCones.clone(rParam = cms.double(0.3), patJetSrc = cms.untracked.InputTag("akPu3PFpatJets"))
+akPu4PFCones = akPu1PFCones.clone(rParam = cms.double(0.4), patJetSrc = cms.untracked.InputTag("akPu4PFpatJets"))
+akPu5PFCones = akPu1PFCones.clone(rParam = cms.double(0.5), patJetSrc = cms.untracked.InputTag("akPu5PFpatJets"))
+akPu6PFCones = akPu1PFCones.clone(rParam = cms.double(0.6), patJetSrc = cms.untracked.InputTag("akPu6PFpatJets"))
+akPu7PFCones = akPu1PFCones.clone(rParam = cms.double(0.7), patJetSrc = cms.untracked.InputTag("akPu7PFpatJets"))
 
-akPu1CaloCones = akPu1PFCones.clone(src = cms.InputTag("towerMaker"), rParam = 0.1, patJetSrc = cms.untracked.InputTag("akPu1CalopatJets"))
-akPu2CaloCones = akPu1PFCones.clone(src = cms.InputTag("towerMaker"), rParam = 0.2, patJetSrc = cms.untracked.InputTag("akPu2CalopatJets"))
-akPu3CaloCones = akPu1PFCones.clone(src = cms.InputTag("towerMaker"), rParam = 0.3, patJetSrc = cms.untracked.InputTag("akPu3CalopatJets"))
-akPu4CaloCones = akPu1PFCones.clone(src = cms.InputTag("towerMaker"), rParam = 0.4, patJetSrc = cms.untracked.InputTag("akPu4CalopatJets"))
-akPu5CaloCones = akPu1PFCones.clone(src = cms.InputTag("towerMaker"), rParam = 0.5, patJetSrc = cms.untracked.InputTag("akPu5CalopatJets"))
-akPu6CaloCones = akPu1PFCones.clone(src = cms.InputTag("towerMaker"), rParam = 0.6, patJetSrc = cms.untracked.InputTag("akPu6CalopatJets"))
+akPu1CaloCones = akPu1PFCones.clone(src = cms.InputTag("towerMaker"), rParam = cms.double(0.1), patJetSrc = cms.untracked.InputTag("akPu1CalopatJets"))
+akPu2CaloCones = akPu1PFCones.clone(src = cms.InputTag("towerMaker"), rParam = cms.double(0.2), patJetSrc = cms.untracked.InputTag("akPu2CalopatJets"))
+akPu3CaloCones = akPu1PFCones.clone(src = cms.InputTag("towerMaker"), rParam = cms.double(0.3), patJetSrc = cms.untracked.InputTag("akPu3CalopatJets"))
+akPu4CaloCones = akPu1PFCones.clone(src = cms.InputTag("towerMaker"), rParam = cms.double(0.4), patJetSrc = cms.untracked.InputTag("akPu4CalopatJets"))
+akPu5CaloCones = akPu1PFCones.clone(src = cms.InputTag("towerMaker"), rParam = cms.double(0.5), patJetSrc = cms.untracked.InputTag("akPu5CalopatJets"))
+akPu6CaloCones = akPu1PFCones.clone(src = cms.InputTag("towerMaker"), rParam = cms.double(0.6), patJetSrc = cms.untracked.InputTag("akPu6CalopatJets"))
+akPu7CaloCones = akPu1PFCones.clone(src = cms.InputTag("towerMaker"), rParam = cms.double(0.7), patJetSrc = cms.untracked.InputTag("akPu7CalopatJets"))
 
 
-akPu1PFCones.puPtMin = 5
-akPu2PFCones.puPtMin = 10
-akPu3PFCones.puPtMin = 15
-akPu4PFCones.puPtMin = 20
-akPu5PFCones.puPtMin = 25
-akPu6PFCones.puPtMin = 30
+akPu1PFCones.puPtMin = cms.double(5.0)
+akPu2PFCones.puPtMin = cms.double(10.0)
+akPu3PFCones.puPtMin = cms.double(15.0)
+akPu4PFCones.puPtMin = cms.double(20.0)
+akPu5PFCones.puPtMin = cms.double(25.0)
+akPu6PFCones.puPtMin = cms.double(30.0)
+akPu7PFCones.puPtMin = cms.double(35.0)
 
-akPu1CaloCones.puPtMin = 2
-akPu2CaloCones.puPtMin = 4
-akPu3CaloCones.puPtMin = 6
-akPu4CaloCones.puPtMin = 8
-akPu5CaloCones.puPtMin = 10
-akPu6CaloCones.puPtMin = 12
+akPu1CaloCones.puPtMin = cms.double(2.0)
+akPu2CaloCones.puPtMin = cms.double(4.0)
+akPu3CaloCones.puPtMin = cms.double(6.0)
+akPu4CaloCones.puPtMin = cms.double(8.0)
+akPu5CaloCones.puPtMin = cms.double(10.0)
+akPu6CaloCones.puPtMin = cms.double(12.0)
+akPu7CaloCones.puPtMin = cms.double(14.0)
 
 randomCones = cms.Sequence(
     akPu2PFCones+
@@ -66,11 +70,13 @@ randomCones = cms.Sequence(
     akPu4PFCones+
     akPu5PFCones+
     akPu6PFCones+
+    akPu7PFCones+
     akPu2CaloCones+
     akPu3CaloCones+
     akPu4CaloCones+
     akPu5CaloCones+
-    akPu6CaloCones
+    akPu6CaloCones+
+    akPu7CaloCones
     )
 
 randomCones2to5 = cms.Sequence(
