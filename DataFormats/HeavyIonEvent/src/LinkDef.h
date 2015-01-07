f31c4599c694c38589b6243ce294a9a4f36714f1
Steve Sanders <ssanders@ku.edu>
2015-01-07 12:36:45 -0600

    Cleaned up LinkDef.h to remove EPFlattenParams.h reference

--- DataFormats/HeavyIonEvent/src/LinkDef.h
+++ DataFormats/HeavyIonEvent/src/LinkDef.h
@@ -2,10 +2,6 @@
 #include "DataFormats/HeavyIonEvent/interface/CentralityBins.h"
 #define __Cent_Bin_h__
 #endif
-#ifndef ___EPFlattenParams_h__
-#include "DataFormats/HeavyIonEvent/interface/EPFlattenParams.h"
-#define ___EPFlattenParams_h__
-#endif
 
 #ifdef __CINT__
 
@@ -16,6 +12,4 @@
 #pragma link C++ nestedtypedef;
 #pragma link C++ class CentralityBins+;
 #pragma link C++ class CBin+;
-#pragma link C++ class EPFlattenParams+;
-#pragma link C++ class EPFlat+;
 #endif
